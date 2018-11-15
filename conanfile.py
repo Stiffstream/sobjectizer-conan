1bdaf3bd366372225ec65c17af6de516c0824b29
eao197 <eao197@gmail.com>
2018-11-15 11:14:12 +0300

    The first version of conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,49 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class SobjectizerConan(ConanFile):
+    name = "sobjectizer"
+    version = "5.5.23"
+
+    license = "BSD 3-Clause"
+    url = "https://bitbucket.org/sobjectizerteam/conan-so5"
+
+    description = (
+            "A small framework for simplification of development of "
+            "concurrent and event-driven applications in C++ "
+            "by using Actor, Publish-Subscribe and CSP models."
+    )
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+
+    source_subfolder = "sobjectizer"
+
+    def source(self):
+        source_url = "https://sourceforge.net/projects/sobjectizer/files/sobjectizer/SObjectizer%20Core%20v.5.5"
+        tools.get("{0}/so-{1}.zip".format(source_url, self.version))
+        extracted_dir = "so-" + self.version
+        os.rename(extracted_dir, self.source_subfolder)
+
+    def build(self):
+        cmake = CMake(self)
+
+        cmake.definitions['SOBJECTIZER_BUILD_SHARED'] = self.options.shared
+        cmake.definitions['SOBJECTIZER_BUILD_STATIC'] = not self.options.shared
+
+        cmake.configure(source_folder = self.source_subfolder + "/dev")
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        self.copy("*.h", dst="include", src=self.source_subfolder + "/dev")
+        self.copy("*.hpp", dst="include", src=self.source_subfolder + "/dev")
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
