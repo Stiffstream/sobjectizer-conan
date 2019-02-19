103439bc063da8330fd3362f0f07ee5e08de8884
eao197 <eao197@gmail.com>
2019-02-19 12:54:28 +0300

    sobjectizer updated to v.5.5.24.2

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class SobjectizerConan(ConanFile):
     name = "sobjectizer"
-    version = "5.5.24.1"
+    version = "5.5.24.2"
 
     license = "BSD 3-Clause"
     url = "https://github.com/Stiffstream/sobjectizer-conan"
