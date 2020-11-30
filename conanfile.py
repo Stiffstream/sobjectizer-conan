2c7b697ea7cd6577c357bf96a396dc272003c14e
Yauheni Akhotnikau <eao197@gmail.com>
2020-11-30 10:48:00 +0300

    SObjectizer updated to v.5.7.2.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class SobjectizerConan(ConanFile):
     name = "sobjectizer"
-    version = "5.7.1"
+    version = "5.7.2"
 
     license = "BSD 3-Clause"
     url = "https://github.com/Stiffstream/sobjectizer-conan"
